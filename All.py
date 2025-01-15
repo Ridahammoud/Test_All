349edfb2ea9c21a3f213601e31ef768a71aa26f8
Ridahammoud <rida.hammoud@aesma.eu>
2025-01-15 11:16:59 +0100

    All.py

--- All.py
+++ All.py
@@ -94,9 +94,7 @@ def generate_pdf(df, filename="tableau.pdf"):
 st.set_page_config(page_title="Analyse des Interventions", page_icon="📊", layout="wide")
 st.title("📊 Analyse des interventions des opérateurs")
 
-url = 'https://docs.google.com/spreadsheets/d/1-OO3px051thFioWyM-0mezmt2rt84wRj/export?format=xlsx'
-response = requests.get(url)
-fichier_principal = response.content
+fichier_principal = "https://docs.google.com/spreadsheets/d/1-OO3px051thFioWyM-0mezmt2rt84wRj/export?format=xlsx"
 df_principal = charger_donnees(fichier_principal)
 
 if fichier_principal is not None:
